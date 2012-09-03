4ecb273a67cb0f58f737d9258d9ad7fc6ea7ad43
Xavier Claude <contact@xavierclaude.be>
2012-09-03 16:47:34 +0200

    Add all the informations for the preums page

--- tribune/views.py
+++ tribune/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, Http404
+from django.db.models import Sum
 from django.shortcuts import render_to_response
 from django.template import Context, loader
-from models import Preums,PreumsMsg, Blacklist
+from models import Blacklist, Preums, PreumsEquipes, PreumsMsg 
 
 
 def index(request):
@@ -24,16 +25,15 @@ def preums(request,tribune,*args, **kwargs):
         preums_col_name = 'preums'    
     filter_args = { preums_col_name: 1}
     blacklist_login = [bl.login for bl in Blacklist.objects.filter(**filter_args)]
- 
-
-    #print blacklist_login
-        
-    preums_list = Preums.objects.filter(preums_name=hour).order_by('score').reverse()
-
-    #preums_table_data=''.join(['<td>%d</td><td>%s;
+    
+    #Last preums
+    last_preums = PreumsMsg.objects.get(preums_name=hour)
+   
+    #Individual highscore list
+    preums_list = Preums.objects.filter(preums_name=hour).exclude(score = 0).exclude(login__in = blacklist_login).order_by('score').reverse()
     table_data = '';
     i = 1
-    #TODO  /!\ Warning big XSS fault (verify that there is no <script> or <img>...)
+    # TODO  /!\ Warning big XSS fault (verify that there is no <script> or <img>...)
     for preums in preums_list:
         if preums.get_equipe():
             name= "%s (%s)" % (preums.login,preums.get_equipe())
@@ -42,9 +42,29 @@ def preums(request,tribune,*args, **kwargs):
         table_data += "<tr><td>%d</td><td>%s</td><td>%d</td></tr>\n" % (i,name,preums.score)
         i+=1
     
+    #Team highscore list
+    team_preums = Preums.objects.filter(preums_name=hour).exclude(equipe = 0).values('equipe').annotate(Sum('score')).order_by('score__sum').reverse()
+    team_table = ''
+    i = 1
+    # TODO  /!\ Warning big XSS fault (verify that there is no <script> or <img>...)
+    for preums in team_preums:
+        team = PreumsEquipes.objects.get(equipe_id=preums['equipe'])
+        team_table += "<tr><td>%d</td><td>%s</td><td>%d</td></tr>\n" % (i,team.nom, preums['score__sum'])
+        i+=1
+    
+    #Stats
+    nb_moules = len(preums_list)
+    nb_team = len(team_preums)
+    nb_preums = preums_list.aggregate(Sum('score'))['score__sum']
+    
     return render_to_response('tribune/preums.djhtml',
                               {'table_data': table_data,
+                               'team_table': team_table,
                                'preums_available': preums_available,
+                               'last_preums': last_preums,
                                'blacklist': blacklist_login,
+                               'nb_moules': nb_moules,
+                               'nb_team': nb_team,
+                               'nb_preums': nb_preums,
                                'tribune': tribune,
                                'hour': hour})
